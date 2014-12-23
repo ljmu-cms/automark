38eaad6b31347f7d8f5ac78acf224e1c56501610
David Llewellyn-Jones <david@flypig.co.uk>
2014-12-23 16:33:43 +0000

    Updated comment identification to use regex. Added list of feedback entries.

--- automark.py
+++ automark.py
@@ -14,6 +14,7 @@ from SOAPpy import WSDL
 import time
 import re
 import random
+import plyj.parser as plyj
 
 class Automark:
 	def __init__(self, filename, credentialsFile):
@@ -23,10 +24,16 @@ class Automark:
 			self.password = file.readline().rstrip('\n')
 			self.score = 0
 			self.filename = filename
-			
+	
 		# Load in the program from file
 		self.program = ""
+		self.lineNumber = []
+		self.errorList = []
+		self.lineCharacterStart = []
 		foundMain = False
+		linesRead = 1
+		linesAdded = 0
+		characterPos = 0
 		with open(filename) as file:
 			for line in file.xreadlines():
 				if not line.startswith('package '):
@@ -36,6 +43,17 @@ class Automark:
 						foundMain = True
 					if not (line.isspace() or (len(line) == 0)):
 						self.program += line
+						self.lineNumber.append(linesRead)
+						self.lineCharacterStart.append(characterPos)
+						linesAdded += 1
+						characterPos += len(line)
+				linesRead += 1
+
+		self.programLines = self.program.splitlines()
+		
+		parser = plyj.Parser()
+		tree = parser.parse_string(self.program)
+		#print tree
 
 		#print self.program
 		# Initialise the scoring state
@@ -48,6 +66,8 @@ class Automark:
 		self.variablesScore = self.checkVariableNameQuality()
 		self.indentationScore = self.checkIndentation()
 		self.executionScore = self.checkExecution()
+		
+		print self.getErrorList()
 
 		print
 		print 'Final score: {:d}'.format(self.getTotalScore())
@@ -96,6 +116,21 @@ class Automark:
 		elif status == 3:
 			print 'Running'
 
+	def getErrorList(self):
+		errorList = ""
+		for error in self.errorList:
+			errorList += str(error[0]) + ' : ' + error[1] + '\n'
+		return errorList
+
+	def lineFromCharacterNoSpace(self, charPos):
+		line = 0
+		while (line < len(self.lineCharacterStart)) and (charPos >= self.lineCharacterStart[line]):
+			line += 1
+		return line
+
+	def lineFromCharacter(self, charPos):
+		return self.lineNumber[self.lineFromCharacterNoSpace(charPos) - 1]
+
 	# Prints output and gives result
 	# True - Success; the output appears correct
 	# False - Failure; the output looks incorrect
@@ -126,62 +161,47 @@ class Automark:
 		return outputScore
 
 	def checkCommentQuality(self):
-		lines = self.program.splitlines()
-		commentCount = 0.0
-		gapSum = 0.0
-		gapAve = 0.0
-		inCommentBlock = False
-		linesSinceLast = -1
-		for line in lines:
-			linesSinceLast += 1
-			if (line.find('/*') >= 0) or (line.find('//') >= 0):
-				if (inCommentBlock == False):
-					# This is the start of a new comment
-					gapSum += linesSinceLast
-					linesSinceLast = 0
-					commentCount += 1
-				inCommentBlock = True
-			if line.find('*/') >= 0:
-				# End of a block comment
-				inCommentBlock = False
-				linesSinceLast = 0
-			if (inCommentBlock == True) and (line.find('//') < 0):
-				inCommentBlock = False
-				linesSinceLast = 0
-		gapSum += linesSinceLast
-		if commentCount > 0:
-			gapAve = gapSum / commentCount
-		else:
-			gapAve = 1000
-			commentCount = 1
+		# Regex expressions search for block comments or full-line comments.
+		# Multiple full-line comments without other text are considered as a single match
+		blockComments = list(re.finditer(r'/\*.*?\*/|//.*?$(?!\s*//)', self.program, (re.MULTILINE | re.DOTALL)))
 
-		gapSumSquared = 0.0
-		gapSD = 0.0
-		inCommentBlock = False
-		linesSinceLast = -1
-		for line in lines:
-			linesSinceLast += 1
-			if (line.find('/*') >= 0) or (line.find('//') >= 0):
-				if (inCommentBlock == False):
-					# This is the start of a new comment
-					gapSumSquared += (linesSinceLast - gapAve)**2.0
-					linesSinceLast = 0
-				inCommentBlock = True
-			if line.find('*/') >= 0:
-				# End of a block comment
-				inCommentBlock = False
-				linesSinceLast = 0
-			if (inCommentBlock == True) and (line.find('//') < 0):
-				inCommentBlock = False
-				linesSinceLast = 0
-		gapSumSquared += (linesSinceLast - gapAve)**2.0
-		gapSD = (gapSumSquared / commentCount)**0.5
+		gapAve = 1000.0
+		gapSD = 1000.0
+		lastCommentLine = 0
+		commentCount = len(blockComments)
+		if commentCount > 0:
+			gapSum = 0
+			previousEnd = 0
+			for blockComment in blockComments:
+				gapSum += self.lineFromCharacterNoSpace(blockComment.start()) - previousEnd
+				previousEnd = self.lineFromCharacterNoSpace(blockComment.end()) + 1
+			gapSum += len(self.programLines) - previousEnd
+			gapAve = gapSum / float(commentCount)
+			print 'New comment count: ' + str(commentCount)
+			print 'New gap: ' + str(gapSum)
+			print 'New gap ave: ' + str(gapAve)
+
+			gapSumSquared = 0.0
+			previousEnd = 0
+			for blockComment in blockComments:
+				gapSumSquared += ((self.lineFromCharacterNoSpace(blockComment.start()) - previousEnd) - gapAve)**2.0
+				previousEnd = self.lineFromCharacterNoSpace(blockComment.end()) + 1
+			gapSumSquared += ((len(self.programLines) - previousEnd) - gapAve)**2.0
+			gapSD = (gapSumSquared / commentCount)**0.5
+			print 'New gap squared: ' + str(gapSumSquared)
+			
+			lastCommentLine = self.lineFromCharacter(blockComments[commentCount - 1].end())
+
 		print 'Comment stats. Gap average: {:f}. Gap SD: {:f}'.format(gapAve, gapSD)
-		commentScore = 0.0
-		commentScore += max(1.0 - ((max(gapAve - 3.0, 0.0))/2.0), 0.0)
-		commentScore += max(1.0 - ((max(gapSD - 2.0, 0.0))/1.0), 0.0)
-		commentScore = int(round(commentScore))
-		print 'Comment score: {:d}'.format(commentScore)
+		commentFrequency = max(1.0 - ((max(gapAve - 3.0, 0.0))/2.0), 0.0)
+		commentConsistency = max(1.0 - ((max(gapSD - 2.0, 0.0))/1.0), 0.0)
+		commentScore = int(round(commentFrequency + commentConsistency))
+		#print 'Comment score: {:d}'.format(commentScore)
+
+		if commentFrequency < 0.75:
+			self.errorList.append([lastCommentLine, 'Try to include more useful comments in your code'])
+		if commentConsistency < 0.75:
+			self.errorList.append([lastCommentLine, 'Your comments should be included throughout your code'])
 		return commentScore
 
 	def getVariableName(self, line):
@@ -194,41 +214,47 @@ class Automark:
 		return name
 
 	def checkVariableNameQuality(self):
-		lines = self.program.splitlines()
 		strike = 0
 		name = ''
-		for line in lines:
+		lineCount = 0
+		for line in self.programLines:
 			name = self.getVariableName(line)
 			if len(name) > 0:
 				if len(name) < 3:
 					strike += 1
-					print name
+					if (strike == 3):
+						self.errorList.append([self.lineNumber[lineCount], 'Use clear and expressive variable names'])
 				if re.search(r'\d+', name) != None:
 					if int(re.search(r'\d+', name).group()) > 0:
 						strike += 1
-						print name
-		print 'Variable name strikes: {:d}'.format(strike)
+						if (strike == 3):
+							self.errorList.append([self.lineNumber[lineCount], 'Use clear and expressive variable names'])
+			lineCount += 1
+		#print 'Variable name strikes: {:d}'.format(strike)
 		variablesScore = 1
 		if strike >= 3:
 			variablesScore = 0
-		print 'Variable name score: {:d}'.format(variablesScore)
+		#print 'Variable name score: {:d}'.format(variablesScore)
 		return variablesScore
 
 	def checkIndentation(self):
-		lines = self.program.splitlines()
 		indentationErrors = 0
 		indent = 0
 		lineNum = 0
-		for line in lines:
+		preChange = 0
+		for line in self.programLines:
 			add = line.count('{')
 			sub = line.count('}')
 			tabs = 0
 			while line[tabs] == '\t':
 				tabs += 1
 			indent -= sub
-			if indent != tabs:
-				indentationErrors += 1
-				print 'Indentation error {:d} (actual) vs. {:d} (expected) on line {:d}'.format(tabs, indent, lineNum)
+			if (indent != tabs):
+				if (add == 0) and (sub == 0) and (tabs != preChange):
+					indentationErrors += 1
+					if indentationErrors > 4:
+						self.errorList.append([self.lineNumber[lineNum], 'Indentation error'])
+					preChange = tabs
 				indent = tabs
 			indent += add
 			lineNum += 1
@@ -242,6 +268,7 @@ class Automark:
 
 	def checkExecution(self):
 		executionScore = 0
+		return executionScore
 		# Creating wsdl client
 		wsdlObject = WSDL.Proxy('http://ideone.com/api/1/service.wsdl')
 
